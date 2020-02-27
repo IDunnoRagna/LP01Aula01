b21628fe0d288f345935ecab73248ac36526b98a
IDunnoRagna <afonsomnmteixeira@gmail.com>
2020-02-27 17:21:41 +0000

    Added TiposVarios

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int i = 0b0010_1001;
+            char symbol = '\u058D';
+            char invert = '\u058E';
+            bool condition = 6 < 2;
+            bool real = 9 > 2;
+            double amount = 1984.2 - 34.82;
+            {
+                Console.WriteLine(symbol);
+                Console.WriteLine(i);
+                Console.WriteLine(invert);
+                Console.WriteLine(condition);
+                Console.WriteLine(real);
+                Console.WriteLine(amount);
+            }
+        }
+    }
+}
