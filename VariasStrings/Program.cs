f4b37d2c7eca1ba6fdabbabebb581c8a55b6a026
IDunnoRagna <afonsomnmteixeira@gmail.com>
2020-02-27 17:51:00 +0000

    Added more strings to VariasStrings

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -9,10 +9,22 @@ namespace VariasStrings
             string c = "awwww\t man so we back in the mine";
             string f = "awwww\t man so we back in the u0488";
             string g = @"C:\awwww man\t so we back in the u0488";
+            string k = "awwww" + "man" + "so we back in the mine";
+            int x = 0b0010_1001;
+            int y = 0b1101_1110;
+            string p = "awwww" + x + "so we back in the" + y;
+            string m = "awwww man" + "so we back in the {x + y}";
+            string z;
+
             {
                 Console.WriteLine(c);
                 Console.WriteLine(f);
                 Console.WriteLine(g);
+                Console.WriteLine(k);
+                Console.WriteLine(p);
+                Console.WriteLine(m);
+                Console.WriteLine("awww man so we back in the {0}", z);
+
             }
         }
     }
