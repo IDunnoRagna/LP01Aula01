4bb0e89b5e0ade2b49e5874bb4393f40e56cee63
IDunnoRagna <afonsomnmteixeira@gmail.com>
2020-02-27 17:36:05 +0000

    Added VariasStrings

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string c = "awwww\t man so we back in the mine";
+            string f = "awwww\t man so we back in the u0488";
+            string g = @"C:\awwww man\t so we back in the u0488";
+            {
+                Console.WriteLine(c);
+                Console.WriteLine(f);
+                Console.WriteLine(g);
+            }
+        }
+    }
+}
